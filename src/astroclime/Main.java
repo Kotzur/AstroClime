7e7376993530edf491f444b71ceaf69f46fbfb02
Kotzur <kocagnes@gmail.com>
2017-05-11 14:43:01 +0100

    Main class to run opening screen and handling closing

--- src/astroclime/Main.java
+++ src/astroclime/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("fxml/test.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("scenes/test.fxml"));
 		primaryStage.setTitle("Welcome");
 
 		primaryStage.setOnCloseRequest(e -> {
