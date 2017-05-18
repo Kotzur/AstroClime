5886952f810a15c7df22a79d339debb02ac56c1a
Toby <t.lloyddavies@yahoo.co.uk>
2017-05-18 13:24:41 +0100

    Added previous location and language lists to WeatherData. Added function to check if a string is a valid city in WeatherData. Updated initialize function in SettingsController. Added button to change location in SettingsView.fxml.

--- src/astroclime/controllers/SettingsController.java
+++ src/astroclime/controllers/SettingsController.java
@@ -1,19 +1,25 @@
 package astroclime.controllers;
 
     import astroclime.backend.WeatherData;
+    import com.jfoenix.controls.JFXButton;
     import com.jfoenix.controls.JFXComboBox;
     import com.jfoenix.controls.JFXRadioButton;
+    import javafx.beans.value.ChangeListener;
+    import javafx.beans.value.ObservableValue;
+    import javafx.collections.FXCollections;
+    import javafx.collections.ObservableList;
+    import javafx.event.EventHandler;
     import javafx.fxml.FXML;
     import javafx.scene.control.ToggleGroup;
     import javafx.event.ActionEvent;
 
-    public class SettingsController {
+public class SettingsController {
 
         @FXML
-        private JFXComboBox<?> location;
+        private JFXComboBox<String> locationBox;
 
         @FXML
-        private JFXComboBox<?> language;
+        private JFXComboBox<String> languageBox;
 
         @FXML
         private ToggleGroup units;
@@ -28,15 +34,51 @@ package astroclime.controllers;
         @FXML
         private JFXRadioButton kelvin;
 
-        //TODO: add logic to check location is valid
         @FXML
-        void changeLocation(ActionEvent event) {
+        private JFXButton changeLocationButton;
+
+        private ObservableList<String> locationOptions;
+        private ObservableList<String> languageOptions;
+
+
+        public void initialize() {
+                celcius.setSelected(WeatherData.CELCIUS);
+
+                locationOptions = FXCollections.observableArrayList(WeatherData.PREVIOUS_LOCATIONS);
+                locationOptions.add(WeatherData.CITY_NAME);
+                FXCollections.reverse(locationOptions);
+                locationBox.setItems(locationOptions);
+                locationBox.getSelectionModel().selectFirst();
+                locationBox.valueProperty().addListener(new ChangeListener<String>() {
+                    @Override
+                    public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                        //TODO: add to front of list
+                        if(!WeatherData.checkValidCity(newValue)){
+                            locationBox.getSelectionModel().select(oldValue);
+                        } else {
+                            locationOptions.add(oldValue);
+                        }
+                }});
+
+                languageOptions = FXCollections.observableArrayList(WeatherData.LANGUAGES);
+                languageBox.setItems(languageOptions);
+                languageBox.getSelectionModel().select(WeatherData.LANGUAGE);
+                //:
+                changeLocationButton.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        locationBox.getSelectionModel().clearSelection();
+                        locationBox.setEditable(true);
+                    }
+                });
 
         }
 
+
+        //:TODO link exit button with this fuction
         private void onExit() {
             WeatherData.CELCIUS = celcius.isSelected();
-            WeatherData.CITY_NAME = location.getValue().toString();
+            WeatherData.CITY_NAME = locationBox.getValue().toString();
         }
 
 
